b804fb7b97a0d40d7c7a5705464fc81e7d3f2d86
Jean Caiza <jeanmcm@hotmail.com>
2025-07-27 12:40:06 -0500

    add modules

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { TrpcModule } from './trpc/trpc.module';
 import { ExampleModule } from './example/example.module';
 import { DatabaseModule } from '@/database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [TrpcModule, ExampleModule, DatabaseModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+      isGlobal: true,
+    }),
+    TrpcModule,
+    DatabaseModule,
+    ExampleModule,
+    AuthModule,
+    UserModule,
+  ],
   providers: [],
 })
 export class AppModule {}

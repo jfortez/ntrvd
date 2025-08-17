6404f99be94d641fce76810133916333deddd4a4
Jean Caiza <jeanmcm@hotmail.com>
2025-08-17 12:55:50 -0500

    add form-kit

--- apps/www/src/components/Login.tsx
+++ apps/www/src/components/Login.tsx
@@ -1,14 +1,22 @@
+import { FormKit } from "@form-kit/form-kit";
+import { z } from "zod";
+
 type LoginProps = {
   onSubmit: () => void;
 };
 
+const schema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
 const Login = ({ onSubmit }: LoginProps) => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = (e: z.infer<typeof schema>) => {
+    console.log(e);
     onSubmit();
   };
 
-  return <form onSubmit={handleSubmit}>Login</form>;
+  return <FormKit schema={schema} onSubmit={handleSubmit} />;
 };
 
 export default Login;
